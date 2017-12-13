170f68b84da2f266a63ee0eccfcb91f280d46cc0
Raghunath Koppuravuri <raghunath.k@mx.corpmx.minacs.com>
2017-12-13 15:04:44 +0530

    done

--- app/routes/pir.js
+++ app/routes/pir.js
@@ -16,28 +16,28 @@ module.exports = function(sql,sqlconfig,jwt,config) {
                 if (err) console.log(err);
                 // create Request object
                 var request = new sql.Request();
-                request.input('initiator_name', sql.NVarChar, req.body.pir_details.InitiatorName)
-                request.input('initiator_date', sql.DateTime, new Date(req.body.pir_details.InitiatorDate))
-                request.input('status', sql.VarChar, req.body.pir_details.Status)
-                request.input('email_address', sql.NVarChar, req.body.pir_details.EMAIL_ADDRESS)
-                request.input('proposed_details', sql.NVarChar, req.body.pir_details.ProposedChangedetails)
-                request.input('reason', sql.NVarChar, req.body.pir_details.ReasonForChange)
-                request.input('remarks', sql.NVarChar, req.body.pir_details.Remarks)
-                request.input('attachment_id', sql.NVarChar,req.body.pir_details.password)
+                request.input('initiator_name', sql.NVarChar, req.body.pir_details.initiator_name)
+                request.input('initiator_date', sql.DateTime, new Date(req.body.pir_details.initiator_date))
+                request.input('status', sql.VarChar, req.body.pir_details.status)
+                request.input('email_address', sql.NVarChar, req.body.pir_details.email_address)
+                request.input('proposed_details', sql.NVarChar, req.body.pir_details.proposed_details)
+                request.input('reason', sql.NVarChar, req.body.pir_details.reason)
+                request.input('remarks', sql.NVarChar, req.body.pir_details.remarks)
+                request.input('attachment_id', sql.NVarChar,req.body.pir_details.attachment_id)
+                request.input('pir_id', sql.Int,req.body.pir_details.pir_id)
+                request.input('mode', sql.VarChar,req.body.mode)
                 // request.output('response_pir_guid',sql.Int)
                 // query to the database and get the records
                 request.execute("PIR.RequestDetails_Save").then(function(recordSet) {
-                    if (recordSet == null || recordSet.recordsets[0].length=== 0 || recordSet === undefined )
+                    if (recordSet == null  || recordSet === undefined )
                       {
                        console.log('Not Exists!');
                        return;
                       }
                     else
                       {
-                       
                         data=recordSet.recordsets;
-                        res.send(JSON.stringify(data));
-                        console.log(JSON.stringify(data));
+                        res.send("Record Updated Successfully");
                       }
                     sql.close();
                   }).catch(function (err) {         
@@ -46,5 +46,70 @@ module.exports = function(sql,sqlconfig,jwt,config) {
                 });
             });
         });
+
+        
+        router.get('/GetAllPIRHistory', function (req, res) {
+         
+            // connect to your database
+            sql.close();
+            sql.connect(sqlconfig, function (err) {
+
+                if (err) console.log(err);
+                // create Request object
+                var request = new sql.Request();
+        
+                request.input('STARTROWINDEX', sql.Int,req.param('pagesIndex'))
+                request.input('MAXIMUMROWS', sql.Int,req.param('pageSize'))
+                request.input('GETTOTAL', sql.Bit,1)
+                request.output('TOTALRECORDS',sql.Int)
+              
+                // query to the database and get the records
+                request.execute("PIR.GET_PIRAllHISTORY_RECORDS").then(function(recordSet) {
+                    if (recordSet == null || recordSet.length === 0)
+                        return;
+                    
+                    // res.send(recordset);
+                    data=recordSet.recordsets;
+                    res.send(JSON.stringify(data));
+                    console.log(data);
+                    sql.close();
+                }).catch(function (err) {         
+                    console.log(err);
+                    sql.close();
+                });
+            });
+            
+        });
+
+        router.get('/GetPIRHistorybyID', function (req, res) {
+          console.log(req.param('pir_guid'))
+            // connect to your database
+            sql.close();
+            sql.connect(sqlconfig, function (err) {
+
+                if (err) console.log(err);
+                // create Request object
+                var request = new sql.Request();
+        
+                request.input('pir_guid', sql.UniqueIdentifier,req.param('pir_guid'))
+              
+                // query to the database and get the records
+                request.execute("PIR.GetPIRHistorybyID").then(function(recordSet) {
+                    if (recordSet == null || recordSet.length === 0)
+                        return;
+                    
+                    // res.send(recordset);
+                    data=recordSet.recordsets;
+                    res.send(JSON.stringify(data));
+                    console.log(data);
+                    sql.close();
+                }).catch(function (err) {         
+                    console.log(err);
+                    sql.close();
+                });
+            });
+            
+        });
+
         return router;
     }
